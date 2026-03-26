bc706398139c30e3fd03071048b282f8a31281e1
suriyabalaji21 <suriyabalaji21off@gmail.com>
2026-03-26 15:16:47 +0530

    Print Hello World

---
+++ HelloApp.java
@@ -0,0 +1,10 @@
+/**
+ * @author [SURIYA]
+ * @version UC1: Display "Hello World"
+ */   
+public class HelloApp {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+    }
+}
