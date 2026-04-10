01a7fe7c7aed96250aeed4dc1b9e3938f3a4cde8
suriyabalaji21 <suriyabalaji21off@gmail.com>
2026-04-10 16:44:26 +0530

    UC5: Display Hello with MCL-Args using Enhanced For Loop or Default Message

--- HelloApp.java
+++ HelloApp.java
@@ -1,24 +1,22 @@
-package com.helloapp;
-
+/**
+ * @author [SURIYA]
+ * @version UC5: Display "Hello" with Multiple Command-Line Arguments using Enhanced For Loop or Default Message
+ */   
 public class HelloApp {
     public static void main(String[] args) {
-        // UC4: Check if arguments were provided
-        if (args.length > 0) {
-            StringBuilder sb = new StringBuilder();
-            
-            // Loop through the array to collect all names
-            for (int i = 0; i < args.length; i++) {
-                sb.append(args[i]);
-                
-                // Add a comma and space only between names
-                if (i < args.length - 1) {
-                    sb.append(", ");
-                }
-            }
-            System.out.println("Hello, " + sb.toString() + "!");
-        } else {
-            // Default value if no names are provided
+
+        if (args.length == 0) {
             System.out.println("Hello, World!");
+        } else {
+            StringBuilder names = new StringBuilder();
+
+            for (String name : args) {
+                if (names.length() > 0) {
+                    names.append(", ");
+                }
+                names.append(name);
+            }
+            System.out.println("Hello, " + names.toString() + "!");
         }
     }
 }
